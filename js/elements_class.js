dcb8826c7161933b48fe7b3554794bc9a5f32b12
安達陽太 <yota@MacBook-Pro-3.local>
2020-10-04 15:31:23 +0900

    Start

---
+++ js/elements_class.js
@@ -0,0 +1,9 @@
+// ボタンをクリックしたら、class名が’foo’の要素を取得し、
+// その内容（テキスト）をコンソールに出力する。
+
+function showElements() {
+  var elements = document.getElementsByClassName('foo');
+  for (var i = 0; i < elements.length; i++) {
+    console.log(elements[i].innerText);
+  }
+}
