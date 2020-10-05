1af05c6ce5f00e58c4e36dbd506bef28fd64b1bf
安達陽太 <yota@MacBook-Pro-3.local>
2020-10-05 14:46:46 +0900

    Finish

---
+++ js/exercise.js
@@ -0,0 +1,20 @@
+// 「ノードを追加」のレクチャーで学習したコードを改修、
+// ユーザーがテキストボックスに文字列を入力後
+// ユーザーがボタンを押すと
+// リストの最後の項目として文字列が追加されるプログラムを書いてみましょう。
+
+// 補足
+// テキストボックスの要素を取得するには、getElementByIDを利用できる。
+
+function append() {
+  // テキストボックスの要素を取得する
+  var textBox = document.getElementById('textBox');
+  // li要素を生成
+  var li = document.createElement('li');
+  // テキストノードを生成
+  var text = document.createTextNode(textBox.value);
+  // liタグの要素に、テキストノード textを追加
+  li.appendChild(text);
+  // idがlistsのulタグに、liを追加。具体的には<li>追加文字列</li>が、追加される。
+  lists.appendChild(li);
+}
